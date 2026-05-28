28b11f073d496f7684a17bf710536d6040f43c04
PA Bardina <pierre-alexandre.bardina@clinia.com>
2026-05-28 10:48:49 -0400

    test

--- sandbox/outputs.tf
+++ sandbox/outputs.tf
@@ -1,3 +1,17 @@
+output "cluster" {
+  value = {
+    arn                        = data.aws_eks_cluster.main.arn
+    certificate_authority_data = data.aws_eks_cluster.main.certificate_authority[0].data
+    endpoint                   = data.aws_eks_cluster.main.endpoint
+    name                       = data.aws_eks_cluster.main.name
+    platform_version           = data.aws_eks_cluster.main.platform_version
+    status                     = data.aws_eks_cluster.main.status
+    oidc_issuer_url            = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
+    oidc_provider              = replace(data.aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")
+    cluster_security_group_id  = data.aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  }
+}
+
 output "ecr" {
   value = {
     repository_url  = aws_ecr_repository.nuon.repository_url
--- sandbox/versions.tf
+++ sandbox/versions.tf
@@ -16,9 +16,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-# data "aws_eks_cluster" "main" {
-#   name = var.cluster_name
-# }
+data "aws_eks_cluster" "main" {
+  name = var.cluster_name
+}
 
 # data "aws_eks_cluster_auth" "main" {
 #   name = var.cluster_name
